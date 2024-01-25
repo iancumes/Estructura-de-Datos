bf5fdb8f1aa1701f124fb25a0586a5ee8aec78a8
Ian <ianrodrigocumes@gmail.com>
2024-01-25 12:27:48 -0600

    Interfaz

---
+++ CalculadoraPostfix/src/ICalculator.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public interface ICalculator {
+    
+    public int add(int n1, int n2);// +
+    public int substraction(int n1, int n2);// -
+    public int multiplication(int n1, int n2);// *
+    public int division(int n1, int n2) throws Exception;// /
+    public int residue(int n1, int n2) throws Exception;// %
+    public ArrayList<Character> read(String charactersString) throws Exception;
+    public int solve(ArrayList<Character> elements) throws Exception;
+}
